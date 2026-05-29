b68bb6dd0891840fdcdbfefc0cda6ce771c2d111
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-05-29 15:52:45 +0200

    feat: change create_shield_function

--- level.py
+++ level.py
@@ -4,13 +4,6 @@ from config import *
 class Level:
     """Level class"""
     def __init__(self, shield_group):
-        self.shield_group = shield_group
-
-        self.create_shields()
-
-    def create_shields(self):
-        """Create shields"""
-        margin = (WIDTH - (NUM_SHIELDS * SHIELD_SIZE[0]) - ((NUM_SHIELDS - 1) * SPACE_BETWEEN)) / 2
-
-        for i in range(NUM_SHIELDS):
-            Shield((margin + (i * (SPACE_BETWEEN + SHIELD_SIZE[0])), PLAY_AREA.bottom - SHIELD_BASE_OFFSET_Y), self.shield_group)
+        self.shield_blocks_group = shield_group
+        self.shield = Shield(self.shield_blocks_group)
+        self.shield.create_shield_blocks()
