1533b7d979a77a001704ce99487c870d4f130dc2
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-03 19:24:11 +0200

    add Alien class

---
+++ entities/alien.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Alien(pygame.sprite.Sprite):
+    """Alien class"""
+    def __init__(self, pos, images, bullets, score, alien_group):
+        super().__init__(alien_group)
+        self.images = images
+        self.image = images[0]
+        self.rect = self.image.get_rect(center = pos)
+        self.pos = pygame.Vector2(self.rect.center)
+
+        self.bullets_images = bullets
+        self.score = score
