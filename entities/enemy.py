ff97f88faeae35534beb98241e3bd045096447d1
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-03 19:41:49 +0200

    Refactor project structure

--- entities/enemy.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-class Enemy:
-    """Enemy class"""
-    def __init__(self): 
-        ...
--- level.py
+++
@@ -1,9 +0,0 @@
-from entities.shield import Shield
-from config import *
-
-class Level:
-    """Level class"""
-    def __init__(self, shield_group):
-        self.shield_blocks_group = shield_group
-        self.shield = Shield(self.shield_blocks_group)
-        self.shield.create_shield_blocks()
