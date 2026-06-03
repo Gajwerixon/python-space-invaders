5ce7091090f23c6d8bcead2acf7b7a44cf6ed977
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-03 19:24:36 +0200

    add Effect class

--- entities/effect.py
+++ entities/effect.py
@@ -1,26 +1,5 @@
 import pygame
-from systems import Timer
-
-class EffectManager:
-    """Effect Manager"""
-    def __init__(self, group):
-        self.group = group
-
-    def spaw_bullet_alien_explosion(self, image, pos, duration):
-        """Spawne explosion effect between bullet and alien"""
-        Effect(image, pos, duration, self.group)
-    
-    def spaw_bullet_shield_explosion(self, image, pos, duration):
-        """Spawn explosion effect between bullet and shield"""
-        Effect(image, pos, duration, self.group)
-
-    def spawn_player_bullet_miss_explosion(self, image, pos, duration):
-        """Spaw explosion effect after bullet miss"""
-        Effect(image, pos, duration, self.group, anchor='midtop')
-
-    def spawn_alien_bullet_miss_explosion(self, image, pos, duration):
-        """Spawn explosion effect after alien bullet miss"""
-        Effect(image, pos, duration, self.group, anchor='midbottom')
+from systems.timer_system import TimerSystem
 
 class Effect(pygame.sprite.Sprite):
     """Effect class"""
@@ -30,7 +9,7 @@ class Effect(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         setattr(self.rect, anchor, pos)
 
-        self.timer = Timer(duration)
+        self.timer = TimerSystem(duration)
         self.timer.start()
 
     def update(self, dt):
