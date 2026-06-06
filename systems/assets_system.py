b1d4308b6c8ae0e8d89c45fbd0739a0a345dbc48
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-06 21:38:01 +0200

    feat: improve assets systems

--- systems/assets_system.py
+++ systems/assets_system.py
@@ -10,6 +10,7 @@ class AssetsSystem:
         self.effects = self.load_effects_assets()
         self.player = self.load_player_assets()
         self.font = self.load_font_assets()
+        self.ufo = self.load_ufo_assets()
 
     def load_alien_assets(self):
         """Load alien assets"""
@@ -29,41 +30,41 @@ class AssetsSystem:
 
                     aliens[alien.name][folder.name].append(ready_img)                    
         return aliens
+    
+    def load_asset(self, path, size):
+        """Load assets function"""
+        img = pygame.image.load(path).convert_alpha()
+        img_scale = pygame.transform.scale(img, size)
+        return img_scale
 
     def load_effects_assets(self):
         """Load effects assets"""
         effects = {
-            'player_bullet_fx': pygame.transform.scale(
-                pygame.image.load('assets/entities/effect/player/player_bullet_fx.png'), 
-                MISS_EXPLOSION_FX_SIZE).convert_alpha(),
-            'player_explosion': [pygame.transform.scale(
-                pygame.image.load('assets/entities/effect/player/player_explosion_0.png'), 
-                PLAYER_SIZE).convert_alpha(), pygame.transform.scale(
-                pygame.image.load('assets/entities/effect/player/player_explosion_1.png'), 
-                PLAYER_SIZE).convert_alpha()],
-            'alien_bullet_fx': pygame.transform.scale(
-                pygame.image.load('assets/entities/effect/alien/alien_bullet_fx.png'), 
-                MISS_ALIEN_EXPLOSION_FX_SIZE).convert_alpha(), 
-            'alien_explosion_fx': pygame.transform.scale(
-                pygame.image.load('assets/entities/effect/alien/alien_explosion_fx.png'), 
-                ALIEN_EXPLOSION_FX_SIZE).convert_alpha(),
+            'player_bullet_fx': self.load_asset('assets/entities/effect/player/player_bullet_fx.png', 
+                                                MISS_EXPLOSION_FX_SIZE),
+            'player_explosion': [self.load_asset('assets/entities/effect/player/player_explosion_0.png', 
+                                                PLAYER_SIZE),
+                                 self.load_asset('assets/entities/effect/player/player_explosion_1.png',
+                                                 PLAYER_SIZE)],
+            'alien_bullet_fx': self.load_asset('assets/entities/effect/alien/alien_bullet_fx.png', 
+                                               MISS_ALIEN_EXPLOSION_FX_SIZE), 
+            'alien_explosion_fx': self.load_asset('assets/entities/effect/alien/alien_explosion_fx.png',
+                                                  ALIEN_EXPLOSION_FX_SIZE)
         }
         return effects
     
     def load_player_assets(self):
         """Load player assets"""
         player_assets = {
-            'player_img': pygame.transform.scale(
-                pygame.image.load('assets/entities/player/player.png').convert_alpha(),
-                PLAYER_SIZE
-            ),
-            'player_img_hud': pygame.transform.scale(
-                pygame.image.load('assets/entities/player/player.png').convert_alpha(),
-                SHIP_IMG_SIZE
-            ),
+            'player_img': self.load_asset('assets/entities/player/player.png', PLAYER_SIZE),
+            'player_img_hud': self.load_asset('assets/entities/player/player.png', SHIP_IMG_SIZE)
         }
         return player_assets
     
     def load_font_assets(self):
         """Load font assets"""
         return pygame.font.Font('assets/fonts/font.ttf', FONT_SIZE)
+    
+    def load_ufo_assets(self):
+        """Load ufo assets"""
+        return self.load_asset('assets/entities/ufo/ufo_img.png', (48, 21))
