d72247823eeb7e898d4e1671b9b363de7746841a
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-10 18:40:29 +0200

    add ShieldSystem class

---
+++ systems/shield_system.py
@@ -0,0 +1,19 @@
+from config import *
+from entities.shield_blocks import ShieldBlocks
+
+class ShieldSystem:
+    """Shields class manager"""
+    def __init__(self, group):
+        self.shield_block_group = group
+
+    def create_shield_blocks(self):
+        """Create shields blocks"""
+        start_pos = (100, PLAY_AREA.bottom - 200)
+
+        for shield in range(NUM_SHIELDS):
+            for current_col, shield_row in enumerate(SHIELD_SHAPE):
+                for current_row, shield_block in enumerate(shield_row):
+                    if shield_block == 1:
+                        pos_x = start_pos[0] + SHIELD_BLOCK_SIZE[0] * current_row + shield * SPACE_BETWEEN
+                        pos_y = start_pos[1] + SHIELD_BLOCK_SIZE[1] * current_col
+                        ShieldBlocks((pos_x, pos_y), self.shield_block_group)
