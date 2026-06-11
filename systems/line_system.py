1aea15bd3815170b7dd044f79a30008779c5d691
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-11 13:50:32 +0200

    add LineSystem class;f

--- systems/line_system.py
+++ systems/line_system.py
@@ -1,7 +1,14 @@
 from config import *
 from entities.line_blocks import LineBlocks
 
-class Line:
-    """Line class manager"""
-    def __init__(self):
-        ...
+class LineSystem:
+    """LineBlocks class manager"""
+    def __init__(self, group):
+        self.line_group = group
+
+    def create_line_blocks(self):
+        """Create line blocks"""
+        for line in range(NUM_LINES):
+            pos_x = LINE_SIZE[0] * line
+            pos_y = HUD_BOTTOM_Y - LINE_SIZE[1]
+            LineBlocks((pos_x, pos_y), self.line_group)
