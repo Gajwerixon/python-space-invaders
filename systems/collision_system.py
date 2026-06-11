66ee371de5d2d6f3bb5b87cd5422cc7f8807d2ca
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-11 13:49:52 +0200

    feat: change groups initialization

--- systems/collision_system.py
+++ systems/collision_system.py
@@ -4,17 +4,10 @@ from config import *
 
 class CollisionSystem:
     """Collision System class"""
-    def __init__(self, player, shield_blocks, player_bullets, 
-                 alien_bullets, aliens, effect_system):
-        self.player = player
-        self.player_bullets = player_bullets
-
-        self.aliens = aliens
-        self.alien_bullets = alien_bullets
-
-        self.shield_blocks = shield_blocks
-
+    def __init__(self, groups, effect_system):
+        self.groups = groups
         self.effect_system = effect_system
+        self.events = []
 
     def update(self):
         """Update collision system"""
@@ -28,7 +21,7 @@ class CollisionSystem:
 
     def player_bullet_outside_play_area(self):
         """Player bullet hit play area top"""
-        for bullet in self.player_bullets:
+        for bullet in self.groups['player_bullets']:
             if bullet.rect.top <= PLAY_AREA.top:
                 bullet.kill()
                 self.effect_system.spawn_player_bullet_miss_explosion(
@@ -39,8 +32,8 @@ class CollisionSystem:
     def player_bullets_aliens_collision(self):
         """Bullet and alien collision"""
         collision = pygame.sprite.groupcollide(
-            self.player_bullets, 
-            self.aliens, 
+            self.groups['player_bullets'], 
+            self.groups['aliens'], 
             True, 
             True
         )
@@ -53,7 +46,7 @@ class CollisionSystem:
 
     def player_bullets_shield_collision(self):
         """Player bullet and shield collision"""
-        collision = pygame.sprite.groupcollide(self.player_bullets, self.shield_blocks, True, True)
+        collision = pygame.sprite.groupcollide(self.groups['player_bullets'], self.groups['shields'], True, True)
         for bullet, shields in collision.items():
             shields[0].damage_shield()
             self.effect_system.spaw_bullet_shield_explosion(
@@ -63,7 +56,7 @@ class CollisionSystem:
 
     def alien_bullet_outside_play_area(self):
         """Alien bullet hit the play area bottom"""
-        for bullet in self.alien_bullets:
+        for bullet in self.groups['alien_bullets']:
             if bullet.rect.bottom >= PLAY_AREA.bottom:
                 bullet.kill()
                 self.effect_system.spawn_alien_bullet_miss_explosion(
@@ -73,17 +66,18 @@ class CollisionSystem:
 
     def alien_bullet_player_collision(self):
         """Alien bullet and player collision"""
-        collision = pygame.sprite.groupcollide(self.player, self.alien_bullets, True, True)
+        collision = pygame.sprite.groupcollide(self.groups['player'], self.groups['alien_bullets'], True, True)
         for player, _ in collision.items():
             self.effect_system.spawn_player_explosion(
                 player.rect.center,
                 1.5,
                 0.125,
             )
+            self.events.append('PLAYER_DEAD')
 
     def alien_bullet_shield_collision(self):
         """Alien bullet and shield collision"""
-        collision = pygame.sprite.groupcollide(self.alien_bullets, self.shield_blocks, True, True)
+        collision = pygame.sprite.groupcollide(self.groups['alien_bullets'], self.groups['shields'], True, True)
         for bullet, shields in collision.items():
             shields[0].damage_shield()
             self.effect_system.spawn_alien_bullet_shield_explosion(
