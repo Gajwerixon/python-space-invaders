001aa925240f4b54351caff08568735b247276ea
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-11 13:42:03 +0200

    feat: add image, rect and fill support to line blocks

--- entities/line_blocks.py
+++ entities/line_blocks.py
@@ -4,9 +4,8 @@ from config import *
 
 class LineBlocks(pygame.sprite.Sprite):
     """Line Blocks class"""
-    def __init__(self, groups):
-        super().__init__(groups)
-        self.line_image = pygame.Surface((WIDTH, LINE_WIDTH))
-        self.line_image.fill((255, 255, 255))
-        self.line_rect= self.line_image.get_rect(topleft = (0, HUD_BOTTOM_Y))
-
+    def __init__(self, pos, group):
+        super().__init__(group)
+        self.image = pygame.Surface(LINE_SIZE)
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect(topleft = pos)
