fa43f578eb1b3d7b5850ab71eb19cae7d4e6b7bc
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-12 16:51:27 +0200

    feat: add movement

--- entities/ufo.py
+++ entities/ufo.py
@@ -1,12 +1,20 @@
 import pygame
 
-from config import *
-
 class Ufo(pygame.sprite.Sprite):
     """Ufo class"""
-    def __init__(self, pos, image, score, group):
+    def __init__(self, image, pos, direction_x, speed, score, group):
         super().__init__(group)
         self.image = image
         self.rect = self.image.get_rect(center=pos)
+        self.pos = pygame.Vector2(self.rect.center)
+
+        self.direction_x = direction_x
+        self.speed = speed
+
         self.score = score
-        
+
+    def movement(self, dt):
+        """Movement"""
+        self.pos.x += self.direction_x * self.speed * dt
+        self.rect.centerx = self.pos.x
+        self.rect.centery = self.pos.y
