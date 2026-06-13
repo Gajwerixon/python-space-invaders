f63763febd2bba9a7383f415ba61e8c65ad52514
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-13 17:28:37 +0200

    reorganize constants varibales

--- entities/shield_blocks.py
+++ entities/shield_blocks.py
@@ -1,6 +1,6 @@
 import pygame
 
-from config import *
+from config import SHIELDS, EXPLOSIONS
 from random import randrange
 
 class ShieldBlocks(pygame.sprite.Sprite):
@@ -8,7 +8,7 @@ class ShieldBlocks(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.shield_blocks_group = groups
-        self.image = pygame.Surface(((SHIELD_BLOCK_SIZE)))
+        self.image = pygame.Surface(((SHIELDS['block_size'])))
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect(topleft = pos)
 
@@ -19,10 +19,10 @@ class ShieldBlocks(pygame.sprite.Sprite):
         for shield in self.shield_blocks_group:
             if self.is_within_explosion_radius(shield, explosion_x, explosion_y):
                 if self.is_direct_hit(explosion_x, shield.rect.centerx):
-                    if randrange(0, 9) < DIRECTION_DESTROY_CHANCE:
+                    if randrange(0, 9) < EXPLOSIONS['shield']['direction_destroy_chance']:
                         shield.kill()
                 else:
-                    if randrange(0, 9) < EDGE_DESTROY_CHANCE:
+                    if randrange(0, 9) < EXPLOSIONS['shield']['edge_destroy_chance']:
                         shield.kill()
 
     def is_within_explosion_radius(self, shield, explosion_x, explosion_y):
@@ -31,12 +31,12 @@ class ShieldBlocks(pygame.sprite.Sprite):
         dy = shield.rect.centery - explosion_y
         distance = dx*dx + dy*dy
 
-        return distance < EXPLOSION_RADIUS
+        return distance < EXPLOSIONS['shield']['radius']
     
     def is_direct_hit(self, explosion_x, shield_x):
         """
         Check if the shield is the same (or one block on the left)
         direction as a explosion
         """
-        if explosion_x == shield_x or explosion_x == shield_x - SHIELD_BLOCK_SIZE[0]:
+        if explosion_x == shield_x or explosion_x == shield_x - SHIELDS['block_size'][0]:
             return True
