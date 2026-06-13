4825d02085d2571f6285cc3f26b4ed2a6f59ca16
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-13 17:29:03 +0200

    reorganize constants varibales

--- entities/line_blocks.py
+++ entities/line_blocks.py
@@ -1,11 +1,11 @@
 import pygame
 
-from config import *
+from config import LINES
 
 class LineBlocks(pygame.sprite.Sprite):
     """Line Blocks class"""
     def __init__(self, pos, group):
         super().__init__(group)
-        self.image = pygame.Surface(LINE_SIZE)
+        self.image = pygame.Surface(LINES['size'])
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect(topleft = pos)
