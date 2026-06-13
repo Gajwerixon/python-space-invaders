d8041aab7405cebbd1269cb36de7f733055e500b
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-13 17:29:27 +0200

    reorganize constants varibales

--- entities/player_bullet.py
+++ entities/player_bullet.py
@@ -1,10 +1,10 @@
 import pygame
-from config import *
+from config import PLAYER_BULLETS
 
 class PlayerBullet(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.Surface(PLAYER_BULLET_SIZE)
+        self.image = pygame.Surface(PLAYER_BULLETS['size'])
         self.image.fill('white')
         self.rect = self.image.get_rect(midbottom = pos)
         self.pos = pygame.Vector2(self.rect.center)
@@ -15,5 +15,5 @@ class PlayerBullet(pygame.sprite.Sprite):
 
     def movement(self, dt):
         """Bullet movement"""
-        self.pos.y += -1 * BULLET_SPEED * dt
+        self.pos.y += -1 * PLAYER_BULLETS['speed'] * dt
         self.rect.center = self.pos
