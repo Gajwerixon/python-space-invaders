75847ae38686ae3dfb7460e9c26180c1437f5571
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-18 16:06:19 +0200

    Add shield damage system based on grid positions

---
+++ entities/shield_block.py
@@ -0,0 +1,45 @@
+import pygame
+
+from config import SHIELDS, EXPLOSIONS
+
+class ShieldBlock(pygame.sprite.Sprite):
+    """Shield Blocks class"""
+    def __init__(self, pos, shield_id, grid_position, groups):
+        super().__init__(groups)
+        self.shield_blocks_group = groups
+        self.image = pygame.Surface(((SHIELDS['block_size'])))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect(topleft = pos)
+
+        self.shield_id = shield_id
+        self.grid_position = grid_position
+
+    def damage_shield(self, entity):
+        """Handle shield damage"""
+        target_grid_positions = self.get_target_positions(entity)
+        self.destroy_target_blocks(target_grid_positions)
+
+    def get_target_positions(self, entity):
+        """Convert explosion shape to grid positions"""
+        explosion_shape = EXPLOSIONS['shield_cache_shape'][entity]
+        target_list = []
+
+        for offset_x, offset_y in explosion_shape:
+            target_list.append((
+                self.grid_position[0] + offset_x, 
+                self.grid_position[1] + offset_y
+                )
+            )
+
+        return target_list
+    
+    def destroy_target_blocks(self, target_list):
+        """Remove blocks hit by explosion"""
+        target_set = set(target_list) 
+
+        for block in self.shield_blocks_group:
+            if block.shield_id != self.shield_id:
+                continue
+
+            if block.grid_position in target_set:
+                block.kill()
