9f32529aeb1dad1ca30ab83ded11b79dd6baaf6a
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-06-19 22:11:21 +0200

    add damage_line function

--- entities/line_blocks.py
+++ entities/line_blocks.py
@@ -9,3 +9,7 @@ class LineBlocks(pygame.sprite.Sprite):
         self.image = pygame.Surface(LINES['size'])
         self.image.fill((255, 255, 255))
         self.rect = self.image.get_rect(topleft = pos)
+
+    def damage_line(self):
+        """Handle line damage"""
+        pass
