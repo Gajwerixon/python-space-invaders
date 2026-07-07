9eef2315eeaaf31d8d08114e8860065624276c25
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-07-07 21:35:21 +0200

    feat: add new functions

--- systems/sounds_system.py
+++ systems/sounds_system.py
@@ -4,11 +4,25 @@ class SoundSystem:
     def __init__(self, sounds):
         self.sounds = sounds
 
+    # --- Player ---
     def player_shoot_play(self):
         self.sounds['player']['shoot'].play()
 
+    def player_dead_play(self):
+        self.sounds['player']['dead'].play()
+
+    # --- Aliens ---
     def alien_dead_play(self):
         pass
 
-    def player_dead_play(self):
-        self.sounds['player']['dead'].play()
+    def aliens_movement_play(self):
+        pass
+
+    # --- UFO ---
+    def ufo_movement_play(self):
+        pass
+
+    def ufo_dead_play(self):
+        pass
+
+    # --- UI ---
