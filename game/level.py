d4c486ca4f2dd964fe7337c38370db416115307e
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-07-07 21:32:18 +0200

    feat: add create_level function and fix few bugs

--- game/level.py
+++ game/level.py
@@ -23,54 +23,37 @@ class Level:
         self.score_2 = 0
 
         self.player = None
-        self.shield_system = ShieldSystem(self.groups['shields'])
-        self.line_system = LineSystem(self.groups['lines'])
-        self.effect_system = EffectSystem(
-            self.assets.effects, 
-            self.groups['effects']
-        )
+        self.shield_system = None
+        self.line_system = None
+        self.aliens_system = None
+        self.ufo_system = None
+
+        self.effect_system = EffectSystem(self.assets.effects, self.groups['effects'])
         self.collision_system = CollisionSystem(self.groups, self.effect_system)
-        self.aliens_system = AliensSystem(
-            self.assets.aliens, 
-            self.groups['alien_bullets'], 
-            self.groups['aliens']
-        )
-        self.ufo_system = UfoSystem(self.assets.ufo['image'], self.groups['ufo'])
 
         self.manually_updated_groups = {'aliens', 'ufo'}
 
-        self.initialize_level()
-
-    def initialize_level(self):
-        self.shield_system.create_shield_blocks()
-        self.line_system.create_line_blocks()
-        self.aliens_system.create_alien_formation()
-
-        self.spawn_player_timer.start()
-
     def update(self, dt):
+        """Update level"""
         self.spawn_player_timer.update(dt)
 
-        if self.phase == 'START':
-            self.update_start(dt)
-
-        elif self.phase == 'GAMEPLAY':
-            self.update_gameplay(dt)
-
-        elif self.phase == 'RESET':
-            self.update_reset(dt)
-
-        elif self.phase == 'NEXT_LEVEL':
-            self.update_next_level(dt)
-
-        elif self.phase == 'GAME_OVER':
-            return
+        states = {
+            'START': self.update_start,
+            'GAMEPLAY': self.update_gameplay,
+            'RESET': self.update_reset,
+            'NEXT_LEVEL': self.update_next_level,
+            'GAME_OVER': lambda _: None
+        }
+        
+        states[self.phase](dt)
 
     def draw(self, surface):
+        """Draw elements"""
         for group in self.groups.values():
             group.draw(surface)
 
     def update_start(self, dt):
+        """Update start"""
         self.aliens_system.update(dt)
 
         if self.try_spawn_player():
@@ -78,27 +61,30 @@ class Level:
             self.phase = 'GAMEPLAY'
 
     def update_gameplay(self, dt):
+        """Update gameplay"""
         self.aliens_system.update(dt)
         self.ufo_system.update(dt)
         self.collision_system.update()
         self.update_groups(dt)
-
         self.handle_events()
 
     def update_reset(self, dt):
+        """Update reset"""
         self.update_groups(dt)
 
         if self.try_spawn_player():
             self.phase = 'GAMEPLAY'
 
     def update_next_level(self, dt):
+        """Update next level"""
         self.next_level_timer.update(dt)
         if self.next_level_timer.active:
-            self.player.update(dt)
+            if self.player:
+                self.player.update(dt)
             return
         
         self.handle_next_level()
-        self.phase = 'START'
+        self.phase = 'START'    
 
     def update_groups(self, dt):
         """Update groups except aliens_group"""
@@ -107,45 +93,62 @@ class Level:
                 group.update(dt)
 
     def handle_events(self):
-        """Handle events from collision system"""
+        """Handle events"""
+        self.handle_collision_events()
+        self.handle_aliens_events()
+        self.handle_player_events()
+
+    def handle_collision_events(self):
         for event in self.collision_system.events:
             event_type = event[0]
             if event_type == "PLAYER_DEAD":
                 self.handle_player_dead()
                 self.sound_system.player_dead_play()
-
             elif event_type == 'UFO_DEAD':
                 self.ufo_system.handle_ufo_dead()
                 self.score_1 += event[1]
-
             elif event_type == 'ALIEN_DEAD':
                 self.score_1 += event[1]
                 self.aliens_system.update_speed()
                 self.sound_system.alien_dead_play()
+        
+        self.collision_system.events.clear()
+
+    def handle_aliens_events(self):
+        """Handle aliens events"""
+        for event in self.aliens_system.events:
+            if event == 'ALL_ALIENS_DEAD':
+                self.phase = 'NEXT_LEVEL'
+                self.next_level_timer.start()
+            elif event == 'PLAY_NEXT_SOUND':
+                pass
+        
+        self.aliens_system.events.clear()
+
+    def handle_player_events(self):
+        if not self.player:
+            return
 
         for event in self.player.events:
             if event == 'PLAYER_SHOOT':
                 self.sound_system.player_shoot_play()
             
-            self.player.events.clear()
-
-        for event in self.aliens_system.events:
-            if event == 'ALL_ALIENS_DEAD':
-                self.aliens_system.events.clear()
-                self.phase = 'NEXT_LEVEL'
-                self.next_level_timer.start()
+        self.player.events.clear()
 
     def try_spawn_player(self):
+        """Try spawn new player"""
         if not self.spawn_player_timer.active:
             self.spawn_player()
             return True
-
         return False
 
     def handle_player_dead(self):
-        """Handle 'Player_dead' event"""
-        for bullet in self.player.bullets:
-            bullet.kill()
+        """Handle player dead"""
+        if self.player:
+            for bullet in self.player.bullets:
+                bullet.kill()
+            self.player.kill()
+            self.player = None
 
         self.lives -= 1
         if self.lives > 0: 
@@ -155,18 +158,29 @@ class Level:
             self.phase = 'GAME_OVER'
 
     def spawn_player(self):
+        """Spawn new player"""
         self.player = Player(
-                self.assets.player['player_img'], 
-                self.groups['player_bullets'], 
-                self.groups['player']
-            )
+            self.assets.player['player_img'], 
+            self.groups['player_bullets'], 
+            self.groups['player']
+        )
         
-    def handle_next_level(self):
-        self.player.kill()
-        self.shield_system.shield_block_group.empty()
-        self.line_system.line_group.empty()
-        self.aliens_system.alien_group.empty()
+    def create_level(self):
+        """Create level"""
+        for group in self.groups.values():
+            group.empty()
 
-        self.aliens_system.create_alien_formation()
+        self.shield_system = ShieldSystem(self.groups['shields'])
+        self.line_system = LineSystem(self.groups['lines'])
+        self.aliens_system = AliensSystem(
+            self.assets.aliens, 
+            self.groups['alien_bullets'], 
+            self.groups['aliens']
+        )
+        self.ufo_system = UfoSystem(self.assets.ufo['image'], self.groups['ufo'])
+
         self.shield_system.create_shield_blocks()
         self.line_system.create_line_blocks()
+        self.aliens_system.create_alien_formation()
+
+        self.spawn_player_timer.start()
