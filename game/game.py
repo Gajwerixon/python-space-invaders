e2b81983f9045efd6e188ee9c871e19a97927bce
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-07-07 21:30:23 +0200

    feat: add switches and refactor class

--- game/game.py
+++ game/game.py
@@ -1,11 +1,7 @@
 import pygame
-
-from config import *
-
+from config import WIDTH, HEIGHT
 from systems.assets_system import AssetsSystem
-
 from game.level import Level
-
 from ui.menu import Menu
 from ui.hud import HUD
 from ui.game_over import GameOver
@@ -21,19 +17,25 @@ class Game:
         self.running = True
 
         # Game variable
-        self.mode = 'LEVEL'
+        self.mode = 'MENU'
         self.num_players = None
         self.credit = 0
         self.high_score = 0
 
         # Assets and HUD
         self.assets = AssetsSystem()
+        self.sound_system = SoundSystem(self.assets.sounds)
         self.hud = HUD(self.assets.player['player_img_hud'], self.assets.font, self.assets.font_images)
 
-        # Create groups and game objects
-        self.groups = self.create_groups()
-        self.create_game_objects()
+        self.menu = None
+        self.advance_table = None
+        self.level = None
+        self.game_over = None
+        self.groups = None
 
+        self.switch_to_menu()
+
+    # Main game functions
     def run(self):
         while self.running:
             dt = self.clock.tick(60) / 1000
@@ -42,79 +44,101 @@ class Game:
             self.draw()
 
     def handle_events(self):
+        """Handle game events"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-            elif self.mode == 'MENU':
+
+            elif self.mode == 'MENU' and self.menu:
                 self.menu.handle_events(event)
 
-    def update(self, dt):
-        if self.mode == 'MENU':
-            self.update_menu(dt)
+    # --- Switches ---
+    def switch_to_menu(self):
+        """Switch to MENU mode"""
+        self.level = None
+        self.groups = None
+        self.advance_table = None
+        self.game_over = None
         
-        elif self.mode == 'ADVANCE_TABLE':
-            self.update_advance_table(dt)
+        self.menu = Menu(self.assets.font) # Tworzymy Menu
+        self.mode = 'MENU'
 
-        elif self.mode == 'LEVEL':
-            self.update_level(dt)
+    def switch_to_advance_table(self):
+        """Switch to ADVANCE_TABLE mode"""
+        self.num_players = self.menu.get_num_players
+        self.menu = None
+        
+        self.advance_table = AdvanceTable(self.assets.font, self.assets.aliens, self.assets.ufo['image'])
+        self.mode = 'ADVANCE_TABLE'
 
-        elif self.mode == 'GAME_OVER':
-            self.update_game_over(dt)
+    def switch_to_level(self):
+        """Switch to LEVEL mode"""
+        self.advance_table = None
+        
+        self.groups = self.create_groups()
+        self.level = Level(self.groups, self.assets, self.sound_system)
+        self.level.create_level()
+        self.mode = 'LEVEL'
+
+    def switch_to_game_over(self):
+        """Switch to GAME_OVER mode"""
+        if self.level.score_1 > self.high_score:
+            self.high_score = self.level.score_1
+            
+        self.game_over = GameOver(self.assets.font)
+        self.mode = 'GAME_OVER'
+
+    # --- Update and Draw functions ---
+    def update(self, dt):
+        """Update game"""
+        if self.mode == 'MENU' and self.menu:
+            self.menu.update(dt)
+            if self.menu.selection_confirmed:
+                self.switch_to_advance_table()
+        
+        elif self.mode == 'ADVANCE_TABLE' and self.advance_table:
+            self.advance_table.update(dt)
+            if self.advance_table.continue_to_game:
+                self.switch_to_level()
+
+        elif self.mode == 'LEVEL' and self.level:
+            self.level.update(dt)
+            if self.level.phase == 'GAME_OVER':
+                self.switch_to_game_over()
+
+        elif self.mode == 'GAME_OVER' and self.game_over:
+            self.game_over.update(dt)
+            if self.game_over.start_new_game:
+                self.switch_to_menu()
 
     def draw(self):
+        """Draw elements on screen"""
         self.surface.fill('black')
 
-        if self.mode == 'MENU':
+        if self.mode == 'MENU' and self.menu:
             self.menu.draw(self.surface)
         
-        elif self.mode == 'ADVANCE_TABLE':
+        elif self.mode == 'ADVANCE_TABLE' and self.advance_table:
             self.advance_table.draw(self.surface)
         
-        elif self.mode == 'LEVEL':
+        elif self.mode == 'LEVEL' and self.level:
             self.level.draw(self.surface)
 
-        elif self.mode == 'GAME_OVER':
+        elif self.mode == 'GAME_OVER' and self.game_over:
             self.level.draw(self.surface)
             self.game_over.draw(self.surface)
 
-        self.hud.draw_hud(self.level.score_1, self.level.score_2, self.high_score, 
-                        self.level.lives, self.credit, self.surface)
+        # HUD
+        score_1 = self.level.score_1 if self.level else 0
+        score_2 = self.level.score_2 if self.level else 0
+        lives = self.level.lives if self.level else 3
+        self.hud.draw_hud(score_1, score_2, self.high_score, lives, self.credit, self.surface)
         
         pygame.display.flip()
 
-    def update_menu(self, dt):
-        """Update MENU mode"""
-        self.menu.update(dt)
-        if self.menu.selection_confirmed:
-            self.num_players = self.menu.get_num_players
-            self.mode = 'ADVANCE_TABLE'
-
-    def update_advance_table(self, dt):
-        self.advance_table.update(dt)
-        if self.advance_table.continue_to_game:
-            self.level.initialize_level()
-            self.mode = 'LEVEL'
-
-    def update_level(self, dt):
-        self.level.update(dt)
-        if self.level.phase == 'GAME_OVER':
-            self.mode = 'GAME_OVER'
-
-    def update_game_over(self, dt):
-        self.game_over.update(dt)
-        if self.game_over.start_new_game:
-            self.reset_game()
-
-    def reset_game(self):
-        self.num_players = None
-        self.credit = 0
-
-        self.groups = self.create_groups()
-        self.create_game_objects()
-
-        self.mode = 'MENU'
-
+    # --- Help function ---
     def create_groups(self):
+        """Create groups"""
         return {
             'lines': pygame.sprite.Group(),
             'shields': pygame.sprite.Group(),
@@ -125,10 +149,3 @@ class Game:
             'effects': pygame.sprite.Group(),
             'ufo': pygame.sprite.Group()
         }
-
-    def create_game_objects(self):
-        self.sound_system = SoundSystem(self.assets.sounds)
-        self.menu = Menu(self.assets.font)
-        self.advance_table = AdvanceTable(self.assets.font, self.assets.aliens, self.assets.ufo['image'])
-        self.level = Level(self.groups, self.assets, self.sound_system)
-        self.game_over = GameOver(self.assets.font)
