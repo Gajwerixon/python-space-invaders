3b6a3422532aa3e53f9a415bab8dd90152f66fda
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-07-10 18:22:07 +0200

    EffectSystem class

--- systems/effect_system.py
+++ systems/effect_system.py
@@ -2,7 +2,7 @@ from entities.effect import Effect
 from entities.animated_effect import AnimatedEffect
 
 class EffectSystem:
-    """Effect Manager"""
+    """Effect system class """
     def __init__(self, effect_assets, group):
         self.assets = effect_assets
         self.group = group
@@ -30,35 +30,35 @@ class EffectSystem:
         )
 
     # --- Player ---
-    def player_bullets_aliens_fx(self, pos, duration=0.25):
+    def player_bullets_aliens_fx(self, pos, duration=0.125):
         """Spawn explosion between player_bullets and aliens"""
         self._create_effect('alien_explosion_fx', pos, duration, 'player_bullets_aliens_fx')
     
-    def player_bullets_shield_blocks_fx(self, pos, duration=0.1):
+    def player_bullets_shield_blocks_fx(self, pos, duration=0.125):
         """Spawn explosion between player bullets and shield blocks"""
         self._create_effect('player_bullet_fx', pos, duration, 'player_bullets_shield_blocks')
 
-    def player_bullets_miss_fx(self, pos, duration=0.25):
+    def player_bullets_miss_fx(self, pos, duration=0.125):
         """Spawn explosion after player bullets hit PLAY_AREA top"""
         self._create_effect('player_bullet_fx', pos, duration, 'player_bullets miss', anchor='midtop')
 
-    def player_bullets_alien_bullets_fx(self, pos, duration=0.2):
+    def player_bullets_alien_bullets_fx(self, pos, duration=0.125):
         """Spawn explosion after player bullets hit alien bullets"""
         self._create_effect('player_bullet_fx', pos, duration, 'player_bullets_alien_bullets')
         self._create_effect('alien_bullet_fx', (pos[0], pos[1] - 20), duration, 'player_bullets_alien_bullets')
 
-    def player_bullets_ufo_fx(self, pos, duration=0.5):
+    def player_bullets_ufo_fx(self, pos, duration=0.25):
         """Spawn explosion after player_bullets hit ufo"""
         self._create_effect('ufo_dead', pos, duration, 'player_bullets ufo')
 
     # --- Aliens ---
-    def alien_bullets_miss_fx(self, pos, duration=0.25):
+    def alien_bullets_miss_fx(self, pos, duration=0.125):
         """Spawn explosion effect after alien bullets hit PLAY_AREA bottom"""
         self._create_effect('alien_bullet_fx', pos, duration, 'alien_bullets miss', anchor='midbottom')
 
-    def alien_bullet_shield_fx(self, pos, duration=0.1):
+    def alien_bullet_shield_fx(self, pos, duration=0.125):
         """Spaw explosion between alien bullets and shield blocks"""
-        self._create_effect('alien_bullet_fx', pos, duration, 'alien_bullets shield_blocks', anchor='center')
+        self._create_effect('alien_bullet_fx', pos, duration, 'alien_bullets shield_blocks')
 
     def alien_bullet_player_fx(self, pos, full_duration=1.5, frame_duration=0.125):
         """Spawn explosion AnimtedEffect after player was hit"""
