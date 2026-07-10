9b9f7585fb711959af8ad6e673fcf7d98f03be44
Gajwerixon <maciej.gajewski3772@gmail.com>
2026-07-10 20:08:41 +0200

    PlayerBullet class

--- entities/player_bullet.py
+++ entities/player_bullet.py
@@ -2,6 +2,7 @@ import pygame
 from config import PLAYER_BULLETS
 
 class PlayerBullet(pygame.sprite.Sprite):
+    """Player Bullet class"""
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.Surface(PLAYER_BULLETS['size'])
@@ -10,10 +11,10 @@ class PlayerBullet(pygame.sprite.Sprite):
         self.pos = pygame.Vector2(self.rect.center)
 
     def update(self, dt):
-        """Update bullet"""
+        """Update PlayerBullet"""
         self.movement(dt)
 
     def movement(self, dt):
-        """Bullet movement"""
+        """PlayerBullet movement"""
         self.pos.y += -1 * PLAYER_BULLETS['speed'] * dt
         self.rect.center = self.pos
